89e1dd37ca38fae150b2461203ddc1a87ea3ce79
AnnaChakhvashvili <chaxvashvili12@gmail.com>
2023-04-04 09:58:30 -0700

    day code commit

---
+++ Day2.2/Lesson2.2.py
@@ -0,0 +1,26 @@
+name="ana"
+print(len(name))
+my_sentence="""I live in Georgia 
+i love dancing """
+print(my_sentence)
+knows_programming=True
+if 38>18:
+    print("hello")
+full_name="ana chakhvashvili"
+print(len(full_name))
+print(full_name[6])
+print(full_name[4:9:2])
+print(full_name[-2:-6:-3])
+print("j" in full_name)
+print("a" in full_name)
+print("n" not in full_name)
+print (full_name[4:9])
+print(full_name[2:8:2])
+print(full_name[5:9])
+name="ajax"
+print(name[1:])
+print(name[2:])
+name="cad group"
+print(name[-2:-7:-2])
+print(name.upper())
+print(name.replace(" ","*"))

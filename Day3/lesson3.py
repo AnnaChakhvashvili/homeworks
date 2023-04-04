3c014b0b6f53cf2d18294b3116bb5d2f00bf6dd0
AnnaChakhvashvili <chaxvashvili12@gmail.com>
2023-04-04 12:14:12 -0700

    day3 code commit

---
+++ Day3/lesson3.py
@@ -0,0 +1,32 @@
+my_name="Ana"
+my_surname="Chakhvashvili"
+my_age=21
+my_height=168
+my_weight=55
+my_sentence="aa {1} bb {0} cc {3} dd {4} ee {2}".format(my_name, my_surname, my_age, my_height, my_weight)
+print(my_sentence)
+my_name=input("enter your name: ")
+print(my_name)
+age = 25
+age -= 4
+print("age")
+float=127.8
+new_age=26
+new_height=171.3
+new_weight=57.5
+print(new_age + int(float) + int(new_height) + int(new_weight))
+day1=0
+day2=1
+day3=00
+day4=100
+print("0" + "1" + "00" + "100")
+# თუ ნამრავლი ნაკლებია 180-ზე ნიშნავს წაგებას
+num1= int(input("enter num1: "))
+num2= int(input("enter num2: "))
+product= num1 * num2
+if product > 180:
+    print(product)
+else:
+    print("you lose")
+    
+

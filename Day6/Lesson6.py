88f8f7b566156d936de15fa82fa54d517ccd083a
AnnaChakhvashvili <chaxvashvili12@gmail.com>
2023-04-21 10:27:40 -0700

    day6 code commit

---
+++ Day6/Lesson6.py
@@ -0,0 +1,56 @@
+# list-სია
+# khinkali 2 lari
+# mwvadi 20 lari
+my_list = ["khinkali", "mwvadi"]
+prices = [2,20]
+print(my_list[0] + " " + str(prices[0]))
+print(my_list[1] + " " + str(prices[1]))
+
+# ციკლში-ციკლი
+for x in range(8):
+    for y in range(8):
+        print("Ana")
+# N 3
+my_name = "ANA"
+print(print(my_name[2]))
+
+# N 4
+for z in range (5):
+    for c in range (1):
+        print(str(z+c) + " " + "ANA")
+
+# while - მეთოდი
+total_price = 0
+i=0
+while i < 4:
+    age_of_user = int(input("enter users age: "))
+    if age_of_user >= 6:
+        total_price += 50
+    i += 1
+print(total_price)
+
+# N 6
+x=0
+y=0
+while x > 6:
+    x += 1
+    y += x
+print(y)
+
+
+A="2.25"
+print(float(A))
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
